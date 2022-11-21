026623ea4be6495b2e0ac5dedde3bb0c74e134e9
Jay Bayron <jaybayron400@gmail.com>
2022-11-21 16:09:54 +0000

    directory

--- 02_01b/index.php
+++ 02_01b/index.php
@@ -1,2 +1,11 @@
 <?php
 echo 'Create your list of choices: ' . "\n\n";
+
+$choices = array();
+
+do{
+  $choices[] = readline( 'Add a choice? (type 0 to stop) ');
+}while('0' != end($choices));
+
+array_pop($choices);
+echo 'And the decision is... ' . $choices[array_rand($choices)];
